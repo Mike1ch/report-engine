5450e94d9838233d052b82abdb12ed830bc3c01d
Mike1ch <charleseniola39@gmail.com>
2026-05-05 03:37:54 +0100

    fix server.js via vscode

--- server/server.js
+++ server/server.js
@@ -15,10 +15,11 @@ app.use(cors({
 app.use(express.json())
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) { cb(null, 'uploads/') },
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
   filename: function (req, file, cb) {
-    const timestamp = Date.now()
-    cb(null, timestamp + '-' + file.originalname)
+    cb(null, file.originalname)
   }
 })
 const upload = multer({ storage: storage })
